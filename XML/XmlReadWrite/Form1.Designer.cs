06787a160f8db8b7db03d165e180c4e231e8b7b5
정규화 <kuha0879@gmail.com>
2018-06-22 21:37:28 +0900

    [Module Test] Xml Read/Write

---
+++ XML/XmlReadWrite/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace XmlReadWrite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnXMLWrite = new System.Windows.Forms.Button();
+            this.btnXMLRead = new System.Windows.Forms.Button();
+            this.txtXML = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnDOMWrite = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnXMLWrite
+            // 
+            this.btnXMLWrite.Location = new System.Drawing.Point(26, 20);
+            this.btnXMLWrite.Name = "btnXMLWrite";
+            this.btnXMLWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnXMLWrite.TabIndex = 0;
+            this.btnXMLWrite.Text = "XML 쓰기";
+            this.btnXMLWrite.UseVisualStyleBackColor = true;
+            this.btnXMLWrite.Click += new System.EventHandler(this.btnXMLWrite_Click);
+            // 
+            // btnXMLRead
+            // 
+            this.btnXMLRead.Location = new System.Drawing.Point(26, 51);
+            this.btnXMLRead.Name = "btnXMLRead";
+            this.btnXMLRead.Size = new System.Drawing.Size(75, 23);
+            this.btnXMLRead.TabIndex = 0;
+            this.btnXMLRead.Text = "XML 읽기";
+            this.btnXMLRead.UseVisualStyleBackColor = true;
+            this.btnXMLRead.Click += new System.EventHandler(this.btnXMLRead_Click);
+            // 
+            // txtXML
+            // 
+            this.txtXML.Location = new System.Drawing.Point(32, 151);
+            this.txtXML.Multiline = true;
+            this.txtXML.Name = "txtXML";
+            this.txtXML.Size = new System.Drawing.Size(197, 109);
+            this.txtXML.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnXMLWrite);
+            this.groupBox1.Controls.Add(this.btnXMLRead);
+            this.groupBox1.Location = new System.Drawing.Point(32, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(122, 84);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "xmlReadWrite";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnDOMWrite);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(204, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(122, 84);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "xml DOM";
+            // 
+            // btnDOMWrite
+            // 
+            this.btnDOMWrite.Location = new System.Drawing.Point(26, 20);
+            this.btnDOMWrite.Name = "btnDOMWrite";
+            this.btnDOMWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnDOMWrite.TabIndex = 0;
+            this.btnDOMWrite.Text = "XML 쓰기";
+            this.btnDOMWrite.UseVisualStyleBackColor = true;
+            this.btnDOMWrite.Click += new System.EventHandler(this.btnDOMWrite_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "XML 읽기";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnXMLRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtXML);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnXMLWrite;
+        private System.Windows.Forms.Button btnXMLRead;
+        private System.Windows.Forms.TextBox txtXML;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnDOMWrite;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ XML/XmlReadWrite/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XmlReadWrite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            XmlWrite();
+        }
+
+        void XmlWrite()
+        {
+            using (XmlWriter wr = XmlWriter.Create(@"C:\Temp\Emp.xml"))
+            {
+                wr.WriteStartDocument();
+                wr.WriteStartElement("Employees");
+
+                // Employee#1001
+                wr.WriteStartElement("Employee");
+                wr.WriteAttributeString("Id", "1001");  // attribute 쓰기
+                wr.WriteElementString("Name", "Tim");   // Element 쓰기
+                wr.WriteElementString("Dept", "Sales");
+                wr.WriteEndElement();
+
+                // Employee#1002
+                wr.WriteStartElement("Employee");
+                wr.WriteAttributeString("Id", "1002");
+                wr.WriteElementString("Name", "John");
+                wr.WriteElementString("Dept", "HR");
+                wr.WriteEndElement();
+
+                wr.WriteEndElement();
+                wr.WriteEndDocument();
+            }
+
+        }
+
+        void xmlRead()
+        {
+            using (XmlReader rd = XmlReader.Create(@"C:\Temp\Emp.xml"))
+            {
+                while (rd.Read())
+                {
+                    if (rd.IsStartElement())
+                    {
+                        if (rd.Name == "Employee")
+                        {
+                            // attribute 읽기                            
+                            string id = rd["Id"]; // rd.GetAttribute("Id");
+
+                            rd.Read();   // 다음 노드로 이동        
+
+                            // Element 읽기
+                            string name = rd.ReadElementContentAsString("Name", "");
+                            string dept = rd.ReadElementContentAsString("Dept", "");
+
+                            Console.WriteLine(id + "," + name + "," + dept);
+                            txtXML.Text += String.Format("{0},{1},{2}", id, name, dept) + System.Environment.NewLine;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void btnXMLWrite_Click(object sender, EventArgs e)
+        {
+            XmlWrite();
+        }
+
+        private void btnXMLRead_Click(object sender, EventArgs e)
+        {
+            xmlRead();
+
+        }
+
+        private void btnDOMWrite_Click(object sender, EventArgs e)
+        {
+            // Create the XmlDocument.  
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml("<book genre='novel' ISBN='1-861001-57-5'>" +
+                        "<title>Pride And Prejudice</title>" +
+                        "</book>");
+
+            // Save the document to a file.  
+            doc.Save("data.xml");  
+
+        }
+
+    }
+}
