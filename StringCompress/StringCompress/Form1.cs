3f52a1906c0bfa15e4b1d65e232ac28e11c2c2e8
정규화 <kuha0879@gmail.com>
2018-06-22 21:36:35 +0900

    [Module Test] String Compress

---
+++ StringCompress/StringCompress/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace StringCompress
+{
+    public partial class Form1 : Form
+    {
+#if true
+        public static string Zip(string text)
+        {
+            byte[] buffer = System.Text.Encoding.Unicode.GetBytes(text);
+            MemoryStream ms = new MemoryStream();
+            using (System.IO.Compression.GZipStream zip = new System.IO.Compression.GZipStream(ms, System.IO.Compression.CompressionMode.Compress, true))
+            {
+                zip.Write(buffer, 0, buffer.Length);
+            }
+
+            ms.Position = 0;
+            MemoryStream outStream = new MemoryStream();
+
+            byte[] compressed = new byte[ms.Length];
+            ms.Read(compressed, 0, compressed.Length);
+
+            byte[] gzBuffer = new byte[compressed.Length + 4];
+            System.Buffer.BlockCopy(compressed, 0, gzBuffer, 4, compressed.Length);
+            System.Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, gzBuffer, 0, 4);
+            return Convert.ToBase64String(gzBuffer);
+        }
+
+        public static string UnZip(string compressedText)
+        {
+            byte[] gzBuffer = Convert.FromBase64String(compressedText);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int msgLength = BitConverter.ToInt32(gzBuffer, 0);
+                ms.Write(gzBuffer, 4, gzBuffer.Length - 4);
+
+                byte[] buffer = new byte[msgLength];
+
+                ms.Position = 0;
+                using (System.IO.Compression.GZipStream zip = new System.IO.Compression.GZipStream(ms, System.IO.Compression.CompressionMode.Decompress))
+                {
+                    zip.Read(buffer, 0, buffer.Length);
+                }
+
+                return System.Text.Encoding.Unicode.GetString(buffer, 0, buffer.Length);
+            }
+        }
+#else
+        public static string Zip(string value)
+        {
+            //Transform string into byte[]  
+            byte[] byteArray = new byte[value.Length];
+            int indexBA = 0;
+            foreach (char item in value.ToCharArray())
+            {
+                byteArray[indexBA++] = (byte)item;
+            }
+
+            //Prepare for compress
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            System.IO.Compression.GZipStream sw = new System.IO.Compression.GZipStream(ms, System.IO.Compression.CompressionMode.Compress);
+
+            //Compress
+            sw.Write(byteArray, 0, byteArray.Length);
+            //Close, DO NOT FLUSH cause bytes will go missing...
+            sw.Close();
+
+            //Transform byte[] zip data to string
+            byteArray = ms.ToArray();
+            System.Text.StringBuilder sB = new System.Text.StringBuilder(byteArray.Length);
+            foreach (byte item in byteArray)
+            {
+                sB.Append((char)item);
+            }
+            ms.Close();
+            sw.Dispose();
+            ms.Dispose();
+            return sB.ToString();
+        }
+
+        public static string UnZip(string value)
+        {
+            //Transform string into byte[]
+            byte[] byteArray = new byte[value.Length];
+            int indexBA = 0;
+            foreach (char item in value.ToCharArray())
+            {
+                byteArray[indexBA++] = (byte)item;
+            }
+
+            //Prepare for decompress
+            System.IO.MemoryStream ms = new System.IO.MemoryStream(byteArray);
+            System.IO.Compression.GZipStream sr = new System.IO.Compression.GZipStream(ms,
+                System.IO.Compression.CompressionMode.Decompress);
+
+            //Reset variable to collect uncompressed result
+            byteArray = new byte[byteArray.Length];
+
+            //Decompress
+            int rByte = sr.Read(byteArray, 0, byteArray.Length);
+
+            //Transform byte[] unzip data to string
+            System.Text.StringBuilder sB = new System.Text.StringBuilder(rByte);
+            //Read the number of bytes GZipStream red and do not a for each bytes in
+            //resultByteArray;
+            for (int i = 0; i < rByte; i++)
+            {
+                sB.Append((char)byteArray[i]);
+            }
+            sr.Close();
+            ms.Close();
+            sr.Dispose();
+            ms.Dispose();
+            return sB.ToString();
+        }
+#endif
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            XDocument doc = XDocument.Load(@"C:\Temp\Emp.xml");
+            string val = doc.ToString(SaveOptions.DisableFormatting);
+            val = Zip(val);
+            val = UnZip(val);
+        }
+    }
+}
+
+
+// xml serialize deserialize https://www.codeproject.com/Articles/1163664/Convert-XML-to-Csharp-Object
